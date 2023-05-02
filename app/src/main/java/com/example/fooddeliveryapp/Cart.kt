896e57819a7d13576ac274a72e727b6a94c87244
MeriemAfafHaddou <jm_haddou@esi.dz>
2023-05-02 10:21:56 +0100

    init

--- app/src/main/java/com/example/fooddeliveryapp/Cart.kt
+++ app/src/main/java/com/example/fooddeliveryapp/Cart.kt
@@ -1,14 +1,16 @@
 package com.example.fooddeliveryapp
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
-object Cart {
-    var orders= mutableListOf<CartItem>()
-        private set
-    fun addToCart(item: CartItem) {
-        orders.add(item)
-    }
-
-    fun clear() {
-        orders.clear()
-    }
-}
-
+@Entity(tableName ="cart" , foreignKeys = [
+    ForeignKey(entity=Restaurant::class,
+        parentColumns=["id_restau"],childColumns = ["restaurant"],
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE )])
+data class Cart(
+    @PrimaryKey
+    val id_cart:Int,
+    val restaurant:Int,
+    val orders:MutableList<CartItem>
+)
